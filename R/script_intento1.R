6b88ece6a03a0419154f9a1444e0504ac33bdad6
julgrandvallet <julgrandvallet@gmail.com>
2023-08-10 17:11:05 -0600

    cambios actuales

--- R/script_intento1.R
+++ R/script_intento1.R
@@ -10,6 +10,18 @@
 
 library(scuttle)
 
+# Función auxiliar para detectar el delimitador
+detect_delim <- function(file_path) {
+  first_line <- readLines(file_path, n = 1)
+  delimiters <- c(",", "\t", "|", " ")
+
+  counts <- sapply(delimiters, function(delim) {
+    sum(unlist(strsplit(first_line, split = "")) == delim)
+  })
+
+  return(delimiters[which.max(counts)])
+}
+
 sc_data_import <- function(file) {
   # Verifica si el archivo existe
   if (!file.exists(file)) {
@@ -19,34 +31,26 @@ sc_data_import <- function(file) {
   # Extrae la extensión del archivo
   file_extension <- tools::file_ext(file)
 
-  # Función auxiliar para detectar el delimitador
-  detect_delim <- function(file_path) {
-    first_line <- readLines(file_path, n = 1)
-    delimiters <- c(",", "\t", "|", " ")
-
-    counts <- sapply(delimiters, function(delim) {
-      sum(unlist(strsplit(first_line, split = "")) == delim)
-    })
-
-    return(delimiters[which.max(counts)])
-  }
-
-  # Si es un .csv o .tsv, detecta el delimitador y luego usa scuttle::readSparseCounts para leerlo
-  if (file_extension == "csv" || file_extension == "tsv") {
-    detected_sep <- detect_delim(file)
-    data <- scuttle::readSparseCounts(file, sep = detected_sep)
-    return(data)
-  } else if (file_extension == "h5ad") {
-    demo <- system.file("extdata", file, package = "zellkonverter")
-    sce <- readH5AD(demo)
-    return(sce)
-  } else if (file_extension == "loom") {
-    demo <- system.file("extdata", file, package = "LoomExperiment")
-    scle <- import(demo, type="SingleCellLoomExperiment")
-    return(scle)
-  } else {
-    stop("Tipo de archivo no soportado.")
-  }
+  # Decide cómo procesar el archivo según su extensión
+  switch(file_extension,
+         csv = {
+           detected_sep <- detect_delim(file)
+           return(scuttle::readSparseCounts(file, sep = detected_sep))
+         },
+         tsv = {
+           detected_sep <- detect_delim(file)
+           return(scuttle::readSparseCounts(file, sep = detected_sep))
+         },
+         h5ad = {
+           demo <- system.file("extdata", file, package = "zellkonverter")
+           return(readH5AD(demo))
+         },
+         loom = {
+           demo <- system.file("extdata", file, package = "LoomExperiment")
+           return(import(demo, type="SingleCellLoomExperiment"))
+         },
+         stop("Tipo de archivo no soportado.")
+  )
 }
 
 
