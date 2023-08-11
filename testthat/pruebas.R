8db3133f44c0410b066f7768ce6b01d9ef3088dc
julgrandvallet <julgrandvallet@gmail.com>
2023-08-11 09:23:26 -0600

    Primera prueba

--- testthat/pruebas.R
+++ testthat/pruebas.R
@@ -1,7 +1,7 @@
 test_that("sc_data_import procesa correctamente archivos CSV", {
-  result <- sc_data_import("ruta/a/tu/archivo/test.csv")
+  result <- sc_data_import("./Test_Files/csv_test.csv")
   # Verificar que result es de la clase o tipo esperado
-  expect_true(inherits(result, "alguna_clase_esperada"))
+  expect_true(inherits(result, "Matrix"))
   # Otros asserts para verificar contenido, dimensiones, etc.
 })
 
